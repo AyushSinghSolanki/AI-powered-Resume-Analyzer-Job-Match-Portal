e1dbc7c42aeb85fda6a6c8442a52301165326887
AyushSinghSolanki <ayushsolanki.pis9@gmail.com>
2026-08-03 02:46:55 +0530

    fix: Update server log message to remove URL and simplify output

--- Backend/server.js
+++ Backend/server.js
@@ -38,5 +38,5 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on PORT ${PORT}`);
 });
